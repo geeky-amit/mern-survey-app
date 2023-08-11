4345356f1deee225155e78ba1563b19b6a5c3e74
geeky-amit <singh.amit_007@hotmail>
2023-08-12 04:29:23 +0530

    (feat) - adding header and homepage componants

---
+++ frontend/src/Componants/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+import profileImage from "../../Assets/amit.jpeg";
+import next from "../../Assets/next.svg";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="logo-container">LOGO</div>
+      <div className="profile-container">
+        <img className="img-container" src={profileImage} alt="profile-image" />
+
+        <img className="arrow-container" src={next} alt="next-arrow" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- frontend/src/Componants/Home/Homepage.js
+++ frontend/src/Componants/Home/Homepage.js
@@ -1,7 +1,45 @@
 import React from "react";
+import Header from "../Header/Header";
+import homeIcon from "../../Assets/home-run.svg";
+import barIcon from "../../Assets/bars-solid.svg";
+import userIcon from "../../Assets/user-solid.png";
 
+import "./Homepage.css";
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="side-header">
+          <img className="icon" src={homeIcon} alt="home-icon" />
+          <div className="user-icon-container">
+            <img className="icon user-icon" src={userIcon} alt="user-icon" />
+          </div>
+          <img className="icon" src={barIcon} alt="bar-icon" />
+        </div>
+
+        <div className="home-container">
+          <div className="home-header">
+            <div className="search-container">
+              <div>
+                <p className="survey-text">Survey List</p>
+              </div>
+              <div>
+                <input type="text" name="survey" className="search-icon" />
+              </div>
+            </div>
+            <div className="sort-container">
+              <div className="sort-icon"></div>
+              <div className="filter-icon"></div>
+              <div className="btn-container">
+                <button className="bar-btn">CREATE</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Homepage;
